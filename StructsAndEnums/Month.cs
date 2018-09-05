4229cade054f1c032999fdc5c49ed9f4f6c2ed79
akareiva <austin.kareiva@outlook.com>
2018-09-05 14:02:13 -0400

    Resubmit

---
+++ StructsAndEnums/Month.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StructsAndEnums
+{
+    enum Month
+    {
+        January, Febuary, March, April, May, June, July, August, September, October, november, December
+
+    }
+}
